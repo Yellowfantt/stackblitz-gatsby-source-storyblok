c4cc285d554481b90a9d23c0c673f095e05133d4
Arisa Fukuzaki <25793187+schabibi1@users.noreply.github.com>
2022-05-09 17:44:59 +0200

    test: add set up test suite

---
+++ playground-test/src/pages/index.js
@@ -0,0 +1,61 @@
+import * as React from "react"
+import { Link, graphql } from "gatsby"
+import configuration from '../../gatsby-config'
+import { storyblokInit, apiPlugin, StoryblokComponent, storyblokEditable, useStoryblokState } from "gatsby-source-storyblok"
+import Teaser from '../components/Teaser'
+import Grid from '../components/Grid'
+import Feature from '../components/Feature'
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const sbConfig = configuration.plugins.find((item) => item.resolve === 'gatsby-source-storyblok')
+
+storyblokInit({
+  accessToken: sbConfig.options.accessToken,
+  use: [apiPlugin],
+  components: {
+    teaser: Teaser,
+    grid: Grid,
+    feature: Feature
+  }
+});
+
+const IndexPage = ({ data }) => {
+  console.log(data)
+  let story = data.storyblokEntry
+  story = useStoryblokState(story)
+
+  const components = story.content.body.map(blok => (<StoryblokComponent blok={blok} key={blok._uid} />))
+
+  return (
+    <Layout>
+      <div {...storyblokEditable(story.content)}>
+        <Seo title="Home" />
+        <h1>{story.name}</h1>
+        {components}
+        <p>
+          <Link to="/page-2/">Go to page 2</Link> <br />
+          <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
+          <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
+          <Link to="/using-dsg">Go to "Using DSG"</Link>
+        </p>
+      </div>
+    </Layout>
+  )
+}
+
+export default IndexPage
+
+export const query = graphql`
+  query HomeQuery {
+    storyblokEntry(full_slug: { eq: "gatsby/" }) {
+      content
+      name
+      full_slug
+      uuid
+      id
+      internalId
+    }
+  }
+`
